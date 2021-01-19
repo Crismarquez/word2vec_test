72cfc8d8e7a9ba71b3a2c16a67080211c3a347bb
Cristian Marquez <cristian.dmr10@gmail.com>
2021-01-19 08:36:03 -0500

    continue check the learning rate ...

--- run_glove_spanish.py
+++ run_glove_spanish.py
@@ -23,8 +23,9 @@ ds = textacy.datasets.Wikipedia(lang="es", version="current")
 ds.download()
 ds.info
 
+
 texts = []
-for text in ds.texts(limit=12000):
+for text in ds.texts(limit=1000):
     texts = texts + re.compile(r'\W+', re.UNICODE).split(text)
 
 print("\n Size imported corpus, tockens: ", len(texts))
@@ -51,18 +52,25 @@ co_occurrence_dict = glove.co_occurrence.cooccurrences(corpus, vocabulary, S_WIN
 print('Size of cooccurrences (conexions): ', len(co_occurrence_dict))
 
 # if we want to save the hist cost
+# learinig_rate = 0.0005  - este multiplicador no sive para 8 millones con 26 mil pala
+# learinig_rate = 0.0002 # propuesta 
 print('optimizing theta ...')
-learinig_rate = 0.0005
-hist_cost = []
-for i in range(5):
+learinig_rate = 0.0008
+hist_cost_2 = []
+for i in range(10):
     print(f'started cicle {i}')
     gradient = glove.gradient.gradient_descent_dict(vocabulary, theta, co_occurrence_dict)
     theta = theta - learinig_rate * gradient
+    print(gradient.max())
+    print(gradient.min())
     cost_model = glove.cost_function.cost_glove_dict(vocabulary, theta, co_occurrence_dict)
-    hist_cost.append(cost_model)
+    hist_cost_2.append(cost_model)
     print(f'finished cicle {i}')
 
+
 plt.plot(range(len(hist_cost)), hist_cost)
+plt.plot(range(len(hist_cost_2)), hist_cost_2)
+plt.plot(range(len(hist_cost_3)), hist_cost_3)
 plt.title("Learning")
 plt.xlabel("Iteration")
 plt.ylabel("Cost")
@@ -71,11 +79,15 @@ plt.savefig('learning_hist.png')
 
 # if we do not want to save the hist cost
 print('optimizing theta ...')
+path = 'C:/Users/Cristian Marquez/Documents/Cristian/Academico/Projects/NLP/word2vec_V2/'
+print(glove.cost_function.cost_glove_dict(vocabulary, theta, co_occurrence_dict))
 learinig_rate = 0.0005
 for i in range(2):
     print(f'started cicle {i}')
     gradient = glove.gradient.gradient_descent_dict(vocabulary, theta, co_occurrence_dict)
     theta = theta - learinig_rate * gradient
+    with open(path + 'theta_spanish' + '.pkl', 'wb') as f:
+        pickle.dump(theta, f, pickle.HIGHEST_PROTOCOL)
     print(f'finished cicle {i}')
 
 
@@ -94,10 +106,22 @@ df = df.T
 df.to_csv('glove_V1_spanish.csv')
 
 
+import numpy as np
+central_index = utils.util.find_index('de', vocabulary)
+central_vector = utils.util.find_vector(central_index, theta, DIMENSION)
+
+context_index = utils.util.find_index('la', vocabulary)
+context_vector = utils.util.find_vector(context_index, theta, DIMENSION, central=False)
+
+dot_product = np.dot(context_vector, central_vector)
+dot_product - np.log(co_occurrence_dict[('de', 'la')])
+
+np.log(co_occurrence_dict[('de', 'la')])
+np.log(330000)
+
 # # save and read coocurrence dictionary
-path = 'C:/Users/Cristian Marquez/Documents/Cristian/Academico/Projects/NLP/word2vec_V2/'
-with open(path + 'theta_spanish' + '.pkl', 'wb') as f:
-            pickle.dump(theta, f, pickle.HIGHEST_PROTOCOL)
+# with open(path + 'theta_spanish' + '.pkl', 'wb') as f:
+#             pickle.dump(theta, f, pickle.HIGHEST_PROTOCOL)
 
 with open(path + 'co_occurence_spanish' '.pkl', 'rb') as f:
     co_occurrence_dict = pickle.load(f)
